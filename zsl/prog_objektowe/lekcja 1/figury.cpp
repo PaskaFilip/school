4b0a7887e03b09eb8ea801bc13f3e6b29a80d435
Filip Paska <73016494+PaskaFilip@users.noreply.github.com>
2021-09-09 20:52:18 +0200

    Update figury.cpp

--- zsl/prog_objektowe/lekcja 1/figury.cpp
+++ zsl/prog_objektowe/lekcja 1/figury.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
-class rectangle{
-public:		
-	float a,b;
-	float Pole(float a, float b);
-	float Obwod(float a, float b);
+class rectangle {
+public:
+	int a;
+	int b;
+	float Pole();
+	float Obwod();
 	void showObwod();
 	void showPole();
 };
 
-	void rectangle::showPole(){
-		return a*b;
-	}
-	void rectangle::showObwod(){
-		return (a+b)*2;
-	}
-
+void rectangle::showObwod() {
+	cout << (a + b) * 2 <<endl;
+}
+void rectangle::showPole() {
+	cout << a * b << endl;
+}
 int main(int argc, char** argv) {
-	setlocale(LC_CTYPE,"polish");
+	setlocale(LC_CTYPE, "polish");
 	rectangle prostokat;
+	prostokat.a = 4;
+	prostokat.b = 6;
 	prostokat.showObwod();
 	prostokat.showPole();
 	return 0;
